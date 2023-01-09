375b512a454196080c3bf8711bdff0b83b6d9b6f
AJ <ajthms@yahoo.com>
2023-01-09 00:07:06 -0500

    working ham

--- scripts.js
+++ scripts.js
@@ -16,8 +16,8 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
   IFrameAPI.createController(element, options, callback);
 };
 
-const hamburger = document.querySelector("hamburger");
-const navMenu = document.querySelector("nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
