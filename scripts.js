e854b7254564ed0fbcc197c02da4ae7294e5569d
AJ <ajthms@yahoo.com>
2023-01-08 23:46:36 -0500

    hamburger start

--- scripts.js
+++ scripts.js
@@ -15,3 +15,16 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
   };
   IFrameAPI.createController(element, options, callback);
 };
+
+const hamburger = document.querySelector("hamburger");
+const navMenu = document.querySelector("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
